6bf9f64e348906cc1b8882ae010d219984e0ca67
Manvir <sehmbimanvir@gmail.com>
2020-05-25 22:54:48 +0530

    Initial Commit

---
+++ src/helpers.js
@@ -0,0 +1,58 @@
+const chalk = require('chalk')
+
+const deviceData = [
+  {
+    manufacturer: 'Google',
+    models: [
+      'Pixel XL',
+      'Pixel 2XL',
+      'Pixel 3XL',
+      'Pixel 3aXL',
+      'Pixel 4XL'
+    ]
+  }, {
+    manufacturer: 'OnePlus',
+    models: [
+      'OnePlus 2',
+      'OnePlus X',
+      'OnePlus 7',
+      'OnePlus 7 Pro',
+      'OnePlus 7T'
+    ]
+  }
+]
+
+const helpers = {
+  logError (message) {
+    console.log(chalk.red(message))
+  },
+  logInfo (message) {
+    console.log(chalk.green(message))
+  },
+  generateRandomString (length = 10) {
+    const string = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    const stringLength = string.length
+    let randomString = ''
+    for (let i = 0; i < length; i++) {
+      randomString += string.charAt(helpers.getRandomNumber(0, stringLength))
+    }
+    return randomString
+  },
+  getRandomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+  },
+  getModelAndManufacturer () {
+    let manfRandom = helpers.getRandomNumber(0, deviceData.length)
+    let modelRandom = helpers.getRandomNumber(0, deviceData[manfRandom].models.length)
+    return {
+      manufacturer: deviceData[manfRandom].manufacturer,
+      model: deviceData[manfRandom].models[modelRandom]
+    }
+  },
+  getOSVersion () {
+    const osVersion = helpers.getRandomNumber(7, 10)
+    return osVersion.toFixed(1)
+  }
+}
+
+module.exports = helpers
---
+++ src/truecaller.js
@@ -0,0 +1,131 @@
+const axios = require('axios')
+const inquirer = require('inquirer')
+const fs = require('fs')
+const path = require('path')
+const helpers = require('./helpers')
+
+const configFilePath = path.join(__dirname, '..', 'config.json')
+
+const axiosInstance = axios.create({
+  headers: {
+    clientSecret: 'lvc22mp3l1sfv6ujg83rd17btt',
+    'User-Agent': 'Truecaller/11.7.5 (Android;6.0)',
+    'Content-Type': 'application/json'
+  }
+})
+
+const { generateRandomString, getModelAndManufacturer, getOSVersion } = helpers
+
+const truecaller = {
+  sendOtp: mobile => {
+    const deviceData = truecaller.generateDeviceData(mobile)
+    return axiosInstance.post(`https://account-asia-south1.truecaller.com/v2/sendOnboardingOtp`, deviceData)
+    .then(response => {
+      return response.data
+    }, err => {
+      return err.response.data
+    })
+  },
+
+  generateDeviceData: phoneNumber => {
+    let deviceDetails = getModelAndManufacturer()
+    return {
+      countryCode: "in",
+      dialingCode: 91,
+      installationDetails: {
+        app: {
+          buildVersion: 5,
+          majorVersion: 11,
+          minorVersion: 7,
+          store: "GOOGLE_PLAY"
+        },
+        device: {
+          deviceId: generateRandomString(15),
+          language: "en",
+          manufacturer: deviceDetails.manufacturer,
+          model: deviceDetails.model,
+          osName: "Android",
+          osVersion: getOSVersion(),
+          mobileServices: [
+            "GMS"
+          ]
+        },
+        language: "en"
+      },
+      phoneNumber: phoneNumber.toString(),
+      region: 'region-2',
+      sequenceNo: 2
+    }
+  },
+
+  validateNumber: mobile => {
+    return /^\d{10}$/.test(mobile)
+  },
+
+  verifyOtp: (mobile, requestId, token) => {
+    const postData = {
+      countryCode: "in",
+      dialingCode: 91,
+      phoneNumber: mobile.toString(),
+      requestId,
+      token
+    }
+    return axiosInstance.post(`https://account-asia-south1.truecaller.com/v1/verifyOnboardingOtp`, postData)
+    .then(response => response.data, err => err.response.data)
+  },
+
+  promptOtp: () => {
+    const questions = [{
+      type: 'input',
+      name: 'otp',
+      message: 'Enter an OTP: '
+    }]
+
+    return inquirer.prompt(questions)
+      .then(answer => {
+        if (answer.otp.length < 5) {
+          return truecaller.promptOtp()
+        }
+        return answer.otp
+      })
+  },
+
+  formatSearchResult: (data) => {
+    const { name } = data
+    let emailIndex = data.internetAddresses.findIndex(item => item.service === 'email')
+    const email = emailIndex !== -1 ? data.internetAddresses[emailIndex].id : ''
+    return { name, email }
+  },
+
+  getInstallationId: () => {
+    const data = fs.readFileSync(configFilePath, 'utf8')
+    return JSON.parse(data).installationId
+  },
+
+  searchNumber: mobile => {
+    const installationId = truecaller.getInstallationId()
+    return axiosInstance.get(`https://search5-noneu.truecaller.com/v2/search`, {
+      params: {
+        q: mobile,
+        countryCode: 'IN',
+        type: 4,
+        locAddr: '',
+        placement: 'SEARCHRESULTS,HISTORY,DETAILS',
+        encoding: 'json'
+      },
+      headers: {
+        Authorization: `Bearer ${installationId}`
+      }
+    })
+    .then(response => {
+      return truecaller.formatSearchResult(response.data.data[0])
+    }, err => err.response.data)
+  },
+
+  setInstallationId: (installationId) => {
+    const data = { installationId }
+    fs.writeFileSync(configFilePath, JSON.stringify(data))
+  }
+}
+
+module.exports = truecaller
